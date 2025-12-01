ca7f7658d31e4cee1778f438987bfede42294915
347Johann <167218863+347Johann@users.noreply.github.com>
2025-12-01 20:17:33 +0800

    Update FENReader.js

--- FENReader.js
+++ FENReader.js
@@ -1,4 +1,5 @@
 const customFen = document.getElementById("custom-fen-input");
+
 const pieceMap = {
   // White Pieces
   K: "Chess Pieces/white-king.png",
@@ -7,6 +8,7 @@ const pieceMap = {
   B: "Chess Pieces/white-bishop.png",
   N: "Chess Pieces/white-knight.png",
   P: "Chess Pieces/white-pawn.png",
+
   // Black Pieces
   k: "Chess Pieces/black-king.png",
   q: "Chess Pieces/black-queen.png",
@@ -16,16 +18,42 @@ const pieceMap = {
   p: "Chess Pieces/black-pawn.png"
 };
 
-// === Sets Position from FEN
-function updatePosition(customFen) {
+function updatePosition() {
   document.querySelectorAll(".square").forEach(element => {
     element.innerHTML = "";
   });
 
-  let fen = customFen.split("/");
+  for (let rank = 0; rank < boardPosition.length; rank++) {
+    for (let file = 0; file < boardPosition.length; file++) {
+      if (boardPosition[rank][file] !== " ") {
+        document.getElementById(`square${rank * 8 + file}`)
+          .append(pc(pieceMap[boardPosition[rank][file]]));
+      }
+
+      if (
+        (boardPosition[rank][file].toLowerCase() === "p" ||
+         boardPosition[rank][file].toLowerCase() === "k" ||
+         boardPosition[rank][file].toLowerCase() === "r") &&
+        !isInitialized
+      ) {
+        importantPieceMap[rank * 8 + file] = false;
+      }
+    }
+  }
+
+  isInitialized = true;
+}
+
+// === Sets Position from FEN ===
+function updatePositionFromFEN(customFen) {
+  document.querySelectorAll(".square").forEach(element => {
+    element.innerHTML = "";
+  });
+
+  const fen = customFen.split("/");
 
   for (let rank = 0; rank < fen.length; rank++) {
-    let currFile = 0; // 0-7 instead of 1-8 for indexing
+    let currFile = 0;
 
     for (let square = 0; square < fen[rank].length; square++) {
       let currFENPiece = fen[rank][square];
@@ -34,33 +62,31 @@ function updatePosition(customFen) {
 
       for (let piece in pieceMap) {
         if (currFENPiece === piece) {
-          if ((piece.toLowerCase() === "p" || piece.toLowerCase() === "k" || piece.toLowerCase() === "r") && !isInitialized) {
-            importantPieceMap[currPos] = false;
-          }
-          currSquare.append(pc(pieceMap[piece]))
-          currFile++
-          break
-        }
-        else if (!isNaN(currFENPiece)) {
-          currFile += parseInt(currFENPiece)
-          break
+          currSquare.append(pc(pieceMap[piece]));
+          currFile++;
+          break;
+        } else if (!isNaN(currFENPiece)) {
+          currFile += parseInt(currFENPiece);
+          break;
         }
       }
     }
   }
-  isInitialized = true;
 }
 
 // === Converts boardPosition to FEN ===
 function boardToFEN(pos) {
-  let fen = ""
+  let fen = "";
+
   for (let rank = 0; rank < pos.length; rank++) {
-    let emptyStreak = 0
+    let emptyStreak = 0;
+
     for (let file = 0; file < pos.length; file++) {
       if (pos[rank][file] === " ") {
         emptyStreak++;
+
         if (file + 1 < pos.length) {
-          if (pos[rank][file + 1] != " ") {
+          if (pos[rank][file + 1] !== " ") {
             fen += emptyStreak;
             emptyStreak = 0;
           }
@@ -72,11 +98,40 @@ function boardToFEN(pos) {
         fen += pos[rank][file];
       }
     }
-    if (rank < 7) {
-      fen += "/"
-    }
+
+    if (rank < 7) fen += "/";
   }
-  return fen
+
+  return fen;
 }
 
-updatePosition(boardToFEN(boardPosition));
+function customFenInput(customFen) {
+  const fen = customFen.split("/");
+  isInitialized = false;
+
+  // Reset boardPosition to empty
+  for (let rank = 0; rank < boardPosition.length; rank++) {
+    for (let file = 0; file < boardPosition[rank].length; file++) {
+      boardPosition[rank][file] = " ";
+    }
+  }
+
+  for (let rank = 0; rank < fen.length; rank++) {
+    let currFile = 0;
+
+    for (let square = 0; square < fen[rank].length; square++) {
+      let currFENPiece = fen[rank][square];
+
+      if (isNaN(currFENPiece)) {
+        boardPosition[rank][currFile] = currFENPiece;
+        currFile++;
+      } else {
+        currFile += parseInt(currFENPiece);
+      }
+    }
+  }
+
+  updatePositionFromFEN(boardToFEN(boardPosition));
+}
+
+updatePosition();
